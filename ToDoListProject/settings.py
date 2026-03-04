654b1dc9141b05f32876a7a239b791adfd5996b0
Merlin Xavier <153304407+Merlin26022004@users.noreply.github.com>
2026-03-04 16:35:40 +0530

    Add WhiteNoise middleware for static file handling

--- ToDoListProject/settings.py
+++ ToDoListProject/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'ToDoListProject.urls'
@@ -126,3 +127,4 @@ STATIC_URL = 'static/'
 import os
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
